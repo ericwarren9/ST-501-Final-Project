eb62b13951ad0b0cf7097ec241da2c6188200a7e
ericwarren9 <erwarren@ncsu.edu>
2023-11-28 17:10:59 -0500

    Problem 1 B Info

---
+++ Problem 1 B.R
@@ -0,0 +1,28 @@
+# Purpose: To make table to find the smallest size of n needed with our set parameters
+
+
+# Initialize values -------------------------------------------------------
+
+percentile <- c(.01, .05, .1, .2)
+alpha <- c(.95, .99, .999)
+
+final_table <- expand.grid(percentile = percentile, alpha = alpha)
+value_table <- data.frame()
+temp <- data.frame()
+
+
+# Make for loop for values ------------------------------------------------
+
+for (p in percentile){
+  for (a in alpha){
+    temp <- log(1-a) / log(1-p)
+    value_table <- rbind(value_table, temp)
+  }
+}
+
+
+# Update and make final table ---------------------------------------------
+
+colnames(value_table) <- "n"
+
+(final_table <- cbind(final_table, value_table))
