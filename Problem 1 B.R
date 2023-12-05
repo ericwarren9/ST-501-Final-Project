6cd611763e7072b834f8a03970ad218143b12e3e
ericwarren9 <erwarren@ncsu.edu>
2023-12-05 18:30:20 -0500

    Updated to include the inequality way (what Dr. Josephs and I talked about)

--- Problem 1 B.R
+++ Problem 1 B.R
@@ -3,26 +3,40 @@
 
 # Initialize values -------------------------------------------------------
 
+# install.packages("tidyverse") # Uncomment code if you do not have package already
+library(tidyverse)
+
 percentile <- c(.01, .05, .1, .2)
 alpha <- c(.95, .99, .999)
+n <- 1:1000
 
-final_table <- expand.grid(percentile = percentile, alpha = alpha)
+final_table <- expand.grid(percentile = percentile, alpha = alpha, n = n)
 value_table <- data.frame()
 temp <- data.frame()
 
 
 # Make for loop for values ------------------------------------------------
 
-for (a in alpha){
-  for (p in percentile){
-    temp <- ceiling(log(1-a) / log(1-p))
-    value_table <- rbind(value_table, temp)
+for (n in n){
+  for (a in alpha){
+    for (p in percentile){
+      temp <- ceiling(1 - (1-p)^n -p^n >= a)
+      value_table <- rbind(value_table, temp)
+    }
   }
 }
 
 
 # Update and make final table ---------------------------------------------
 
-colnames(value_table) <- "n"
+colnames(value_table) <- "condition_met"
 
-(final_table <- cbind(final_table, value_table))
+# Get only values where n is met
+(
+  final_table <- cbind(final_table, value_table) %>%
+    filter(condition_met == 1) %>%
+    mutate(Var = map2_chr(percentile, alpha, ~toString(sort(c(.x, .y))))) %>%
+    distinct(Var, .keep_all = TRUE) %>%
+    select(-c(Var, condition_met)) %>%
+    arrange(percentile, alpha)
+)
