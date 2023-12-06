0d58e8c03390002f94552a7ac9b735333decfe63
KevinK28 <108950055+KevinK28@users.noreply.github.com>
2023-12-06 10:52:02 -0500

    2 a ii code (Final)

--- FinalCode.R
+++ FinalCode.R
@@ -1,31 +1,57 @@
-datasets = list()
-for (n in 1:50){
-  datasets_n <- matrix(rexp(1000 * n, rate = 1), nrow = 1000, ncol = n)
-  datasets[[as.character(n)]] <- datasets_n
+# Set the seed for reproducibility
+set.seed(123)
+
+# Parameters
+lambda <- 1
+N <- 1000
+max_n <- 50
+
+# Create a list to store the datasets
+datasets_list <- vector("list", N * max_n)
+
+# Generate N datasets for sample sizes 1 to 50
+counter <- 1
+for (i in 1:N) {
+  for (n in 1:max_n) {
+    # Generate a random sample of size n from exponential distribution
+    dataset <- rexp(n, rate = lambda)
+    
+    # Store the dataset in the list
+    datasets_list[[counter]] <- dataset
+    counter <- counter + 1
+  }
 }
+
+# Create a data frame to store minimum values and sample sizes
+min_values_df <- data.frame(Minimum = numeric(N * max_n), SampleSize = numeric(N * max_n))
+
+# Populate the data frame with minimum values and sample sizes
+counter <- 1
+for (i in 1:N) {
+  for (n in 1:max_n) {
+    # Extract minimum value
+    min_value <- min(datasets_list[[counter]])
+    
+    # Store minimum value and sample size in the data frame
+    min_values_df$Minimum[counter] <- min_value
+    min_values_df$SampleSize[counter] <- n
+    
+    counter <- counter + 1
+  }
+}
+
+min_values_df$prob <- ifelse(abs(min_values_df$Minimum) < 0.05, 0, 1)
+
+install.packages("dplyr")
+library(dplyr)
+
+percentage_zeros <- min_values_df %>%
+  group_by(SampleSize) %>%
+  summarize(PercentageZeros = mean(prob == 0) * 100)
 
 library(ggplot2)
-
-minvalue = data.frame(n = integer(), minval = double())
-for (n in 1:50) {
-  min_values <- apply(datasets[[as.character(n)]], 1, min)
-  minvalue <- rbind(minvalue, data.frame(n = rep(n, 1000), minval = min_values))
-}
-
-install.packages("dyplyr")
-library(dyplyr)
-
-# Assuming your dataset is named 'your_dataset'
-# Replace 'your_dataset' with the actual name of your dataset
-
-# Summarize the data to get the minimum values for each unique 'n'
-summary_data <- minvalue %>%
-  group_by(n) %>%
-  summarize(min_value = min(minval))
-
-# Create a line chart
-ggplot(summary_data, aes(x = n, y = min_value, group = 1)) +
+ggplot(percentage_zeros, aes(x = SampleSize, y = PercentageZeros)) +
   geom_line() +
-  labs(title = "Line Chart of Minimum Values",
-       x = "Sample Size (n)",
-       y = "Minimum Value")
+  labs(title = "P( |X(1) - 0| < 0.05 ) ",
+       x = "Sample Size",
+       y = "Probability")
