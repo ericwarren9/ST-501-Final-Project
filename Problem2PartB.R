ab20e96c67ef6cb8188db26c09d2e5070acc5132
lukefreud <104161263+lukefreud@users.noreply.github.com>
2023-12-07 18:34:49 -0500

    Add files via upload

--- Problem2PartB.R
+++ Problem2PartB.R
@@ -64,23 +64,42 @@ for (i in 1:1000) {
 
 par(mfrow = c(3, 3))
 Hist1 <- hist(means1, col = "Blue", main = "Lambda = 1 n = 5", xlab = "", xlim = c(min(means1),max(means1)))
-##curve(rnorm(data1, mean = means1, sd = 1))
+breaks1 <- Hist1$breaks
+curve(dnorm(x, mean = 1, sd = sqrt(1/5)) * length(means1)*diff(breaks1), add = TRUE, col = "red", lwd = 2)
 
 Hist2 <- hist(means2, col = "Blue", main = "Lambda = 1 n = 10", xlab = "", ylab = "", xlim = c(min(means1),max(means1)))
+breaks2 <- Hist2$breaks
+curve(dnorm(x, mean = 1, sd = sqrt(1/10)) * length(means2)*diff(breaks2), add = TRUE, col = "red", lwd = 2)
 
 Hist3 <- hist(means3, col = "Blue", main = "Lambda = 1 n = 100", xlab = "", ylab = "", xlim = c(min(means1),max(means1)))
+breaks3 <- Hist3$breaks
+curve(dnorm(x, mean = 1, sd = sqrt(1/100)) * length(means3)*diff(breaks3), add = TRUE, col = "red", lwd = 2)
 
 Hist4 <- hist(means4, col = "Blue", main = "Lambda = 5 n = 5", xlab = "", xlim = c(min(means4),max(means4)))
+breaks4 <- Hist4$breaks
+curve(dnorm(x, mean = 5, sd = sqrt(5/5)) * length(means4)*diff(breaks4), add = TRUE, col = "red", lwd = 2)
+
 
 Hist5 <- hist(means5, col = "Blue", main = "Lambda = 5 n = 10", xlab = "", ylab = "", xlim = c(min(means4),max(means4)))
+breaks5 <- Hist5$breaks
+curve(dnorm(x, mean = 5, sd = sqrt(5/10)) * length(means5)*diff(breaks5), add = TRUE, col = "red", lwd = 2)
 
 Hist6 <- hist(means6, col = "Blue", main = "Lambda = 5 n = 100", xlab = "", ylab = "", xlim = c(min(means4),max(means4)))
+breaks6 <- Hist6$breaks
+curve(dnorm(x, mean = 5, sd = sqrt(5/100)) * length(means6)*diff(breaks6), add = TRUE, col = "red", lwd = 2)
 
 Hist7 <- hist(means7, col = "Blue", main = "Lambda = 25 n = 5", xlim = c(min(means7),max(means7)))
+breaks7 <- Hist7$breaks
+curve(dnorm(x, mean = 25, sd = sqrt(25/5)) * length(means7) * diff(breaks7), add = TRUE, col = "red", lwd = 2)
 
 Hist8 <- hist(means8, col = "Blue", main = "Lambda = 25 n = 10", ylab = "", xlim = c(min(means7),max(means7)))
+breaks8 <- Hist8$breaks
+curve(dnorm(x, mean = 25, sd = sqrt(25/10)) * length(means8) * diff(breaks8), add = TRUE, col = "red", lwd = 2)
+
 
 Hist9 <- hist(means9, col = "Blue", main = "Lambda = 25 n = 100", ylab = "", xlim = c(min(means7),max(means7)))
+breaks9 <- Hist9$breaks
+curve(dnorm(x, mean = 25, sd = sqrt(25/100)) * length(means9) * .5, add = TRUE, col = "red", lwd = 2)
 
 Prob1 <- sum(means1 >= 1 + 2*sqrt(1/5)) / length(means1)
 
@@ -99,3 +118,6 @@ Prob7 <- sum(means7 >= 25 + 2*sqrt(25/5)) / length(means7)
 Prob8 <- sum(means8 >= 25 + 2*sqrt(25/10)) / length(means8)
 
 Prob9 <- sum(means9 >= 25 + 2*sqrt(25/100)) / length(means9)
+
+## Normal approximation probability
+normapprox <- 1 - pnorm(2,0,1)
